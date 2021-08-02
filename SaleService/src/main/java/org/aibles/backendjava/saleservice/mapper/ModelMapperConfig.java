04cbd734768effe5b56cf747afa0accaf8720f83
LMThumeo <nilelouis2611@gmail.com>
2021-08-02 10:47:36 +0700

    update: add and config ModelMapper

---
+++ SaleService/src/main/java/org/aibles/backendjava/saleservice/mapper/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package org.aibles.backendjava.saleservice.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
